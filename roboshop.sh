66d18ef2d5e4e4f2e5e3dfb52b37a6f7001f0fbd
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-02 23:01:28 +0530

    new record added

--- roboshop.sh
+++ roboshop.sh
@@ -5,5 +5,27 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
+   $( aws ec2 run-instances 
+   --image-id $AMI_ID \
+   --instance-type "t3.micro" \
+   --security-group-ids $SG_ID \
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+   --query 'Instances[0].InstanceId' \
+   --output text )
 
+   if [ $instance == "Frontend" ]; then
+       IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $instance_id \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+       )
+   else
+       IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $instance_id \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+       )
+   fi        
 done
