5ece812fa63d8afe3bfac0929959f6941a7f7dfc
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-05 23:17:40 +0530

    actions added

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 SG_ID="sg-043976682ebbf8cd8" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z02170423JE1RGHUW68WL"
+DOMAIN_NAME="vishwa88s.online"
 
 for instance in $@
 do
@@ -20,6 +22,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
        )
+       RECORD_NAME="$DOMAIN_NAME" # vishwa88s.online
    else
        IP=$(
             aws ec2 describe-instances \
@@ -27,7 +30,34 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
        )
+       RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
    fi    
 
-   echo "IP Address : $IP"    
+    echo "IP Address : $IP"
+
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Updating record",
+            "Changes": [
+                {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                    {
+                        "Value": "'$IP'"
+                    }
+                    ]
+                }
+                }
+            ]
+        }
+        .
+
+        echo "record updated for $instance" 
+           
 done
