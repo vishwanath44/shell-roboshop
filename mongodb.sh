80da53618cbe79be443002c7a91bd7a329dc70d8
vishwanath44 <chinthavishwanath9@gmail.com>
2026-02-07 12:47:45 +0530

    new code added

--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 USERID=$(id -u)
+SCRIPT_NAME=$(basename "$0")
 LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# Create log directory first
+mkdir -p $LOGS_FOLDER
+
+# Root check
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-mkdir -p $LOGS_FOLDER
-
-VALIDATE(){
+VALIDATE() {
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
@@ -24,20 +28,35 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying Mongo Repo" 
+echo -e "$Y Configuring MongoDB Repository $N" | tee -a $LOGS_FILE
+
+cat <<EOF >/etc/yum.repos.d/mongo.repo
+[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/6.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+EOF
+
+VALIDATE $? "Creating MongoDB repo"
+
+dnf clean all &>>$LOGS_FILE
+dnf makecache &>>$LOGS_FILE
 
 dnf install mongodb-org -y &>>$LOGS_FILE
-VALIDATE $? "Installing MongoDB server"
+VALIDATE $? "Installing MongoDB Server"
 
 systemctl enable mongod &>>$LOGS_FILE
-VALIDATE $? "Enable MongoDB"
+VALIDATE $? "Enabling MongoDB Service"
 
-systemctl start mongod
-VALIDATE $? "Start MongoDB"
+systemctl start mongod &>>$LOGS_FILE
+VALIDATE $? "Starting MongoDB Service"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "Allowing remote connections"
+sed -i 's/^  bindIp:.*$/  bindIp: 0.0.0.0/' /etc/mongod.conf
+VALIDATE $? "Allowing Remote Connections"
 
-systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
+systemctl restart mongod &>>$LOGS_FILE
+VALIDATE $? "Restarting MongoDB Service"
+
+echo -e "$G MongoDB Installation Completed Successfully $N" | tee -a $LOGS_FILE
